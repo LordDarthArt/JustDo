83790b0f9a4dc92ee9f104c371eeb9a35ec08323
LordDarthArt <lorddarthart@gmail.com>
2019-02-01 01:13:05 +0300

    -

---
+++ app/src/main/java/tk/lorddarthart/justdo/AdditionalnfoActivity.kt
@@ -0,0 +1,12 @@
+package tk.lorddarthart.justdo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AdditionalnfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_additional_info)
+    }
+}

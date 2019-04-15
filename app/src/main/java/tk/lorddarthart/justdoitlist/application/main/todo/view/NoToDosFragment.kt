6a13e44650823dab613a1895d8dd849af3d538ef
Artyom Tarasov <tarasov@ittest-team.ru>
2019-04-16 00:01:54 +0300

    Maybe this will be the final refactor?

--- app/src/main/java/tk/lorddarthart/justdoitlist/application/main/todo/view/NoToDosFragment.kt
+++ app/src/main/java/tk/lorddarthart/justdoitlist/application/main/todo/view/NoToDosFragment.kt
@@ -7,8 +7,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_no_todos.view.*
 import tk.lorddarthart.justdoitlist.R
 import tk.lorddarthart.justdoitlist.application.BaseActivity
+import tk.lorddarthart.justdoitlist.application.main.todo.add.AddFragment
 
 
 private const val ARG_PARAM1 = "param1"
@@ -38,7 +40,10 @@ class NoToDosFragment : Fragment() {
         // Inflate the layout for this fragment
         mView = inflater.inflate(R.layout.fragment_no_todos, container, false)
 
-        // TODO: do something
+        mView.button_no_tasks_create_one.setOnClickListener {
+            val fragment = AddFragment()
+            mActivity.supportFragmentManager.beginTransaction().replace(R.id.fragment_main, fragment).commit()
+        }
 
         return mView
     }
